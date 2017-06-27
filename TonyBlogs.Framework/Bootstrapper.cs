5d38335c5ab4b2f5330119ad72eb8e27733f8baf
Shinly <xinl_tong@foxmail.com>
2017-06-27 17:07:52 +0800

    1. 创建数据层

---
+++ TonyBlogs.Framework/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TonyBlogs.Framework
+{
+    public class Bootstrapper
+    {
+        public static ContainerBuilder ContainerBuilder { get; set; }
+
+        static Bootstrapper()
+        {
+            ContainerBuilder = new ContainerBuilder();
+        }
+
+        public void Start()
+        { 
+            Type baseType = typeof(IDependency);
+
+            // 获取所有相关类库的程序集
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            ContainerBuilder.RegisterAssemblyTypes(assemblies)
+                .Where(type => baseType.IsAssignableFrom(type) && !type.IsAbstract)
+                .AsImplementedInterfaces().InstancePerLifetimeScope();//InstancePerLifetimeScope 保证对象生命周期基于请求
+        }
+    }
+}
---
+++ TonyBlogs.IRepository/IBaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TonyBlogs.IRepository
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        #region 查询
+        /// <summary>
+        /// 单表查询
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        List<TEntity> QueryWhere(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity Single(Expression<Func<TEntity, bool>> predicate);
+        #endregion
+
+        #region 编辑
+        /// <summary>
+        /// 通过传入的model加需要修改的数据
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="propertys"></param>
+        void Update(TEntity model, Expression<Func<TEntity, bool>> where);
+
+        /// <summary>
+        /// 修改指定字段
+        /// </summary>
+        /// <param name="model"></param>
+        void UpdateOnly<TKey>(TEntity model, Expression<Func<TEntity, TKey>> onlyFields, Expression<Func<TEntity, bool>> where);
+        #endregion
+
+        #region 删除
+        void Delete(Expression<Func<TEntity, bool>> predicate);
+        #endregion
+
+        #region 新增
+        void Add(TEntity model);
+
+        long Add(TEntity model, bool selectIdentity);
+        #endregion
+    }
+}
