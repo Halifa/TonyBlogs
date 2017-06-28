eab88a836459ff966425fbc497b5c06468b88c4d
Shinly <xinl_tong@foxmail.com>
2017-06-28 20:01:11 +0800

    同步

---
+++ TonyBlogs.Service/BlogArticleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TonyBlogs.Entity;
+using TonyBlogs.IRepository;
+using TonyBlogs.IService;
+
+namespace TonyBlogs.Service
+{
+    public class BlogArticleService : BaseService<BlogArticle>, IBlogArticleService
+    {
+        private IBlogArticleRepository dal;
+
+        public BlogArticleService(IBlogArticleRepository dal)
+        {
+            this.dal = dal;
+            base.baseDal = dal;
+        }
+    }
+}
