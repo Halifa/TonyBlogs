96ab080f5f21ebcdcba505b26727b20c9b59b142
Shinly <xinl_tong@foxmail.com>
2017-06-28 20:01:41 +0800

    同步

---
+++ TonyBlogs.Entity/BlogArticle.cs
@@ -0,0 +1,63 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TonyBlogs.Entity
+{
+    public class BlogArticle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [PrimaryKey]
+        [AutoIncrement]
+        public long ID { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string Submitter { get; set; }
+
+        /// <summary>
+        /// 博客标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 访问量
+        /// </summary>
+        public int Traffic { get; set; }
+
+        /// <summary>
+        /// 评论数量
+        /// </summary>
+        public int CommentNum { get; set; }
+
+        /// <summary> 
+        /// 修改时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
