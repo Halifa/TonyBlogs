350f17be69573e4177aa76bc4e07e69cc60a9e47
Shinly <xinl_tong@foxmail.com>
2017-06-29 19:53:33 +0800

    添加functioncontroller

---
+++ TonyBlogs.WebApp/Areas/Admin/Controllers/FunctionController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TonyBlogs.DTO.UserFunction;
+using TonyBlogs.IService;
+
+namespace TonyBlogs.WebApp.Areas.Admin.Controllers
+{
+    public class FunctionController : Controller
+    {
+        private IUserFunctionService _funcService;
+
+        public FunctionController(IUserFunctionService funcService)
+        {
+            this._funcService = funcService;
+        }
+
+        public ActionResult Index()
+        {
+            var list = _funcService.GetAllValidFunciton();
+
+            return View(list);
+        }
+
+        public ActionResult AjaxGetAllFunctions()
+        {
+            var list = _funcService.GetAllValidFunciton();
+
+            return PartialView(list);
+        }
+
+        public ActionResult AjaxGetFuncEditDTO(long funcID)
+        {
+            var result = _funcService.GetFunctionEditDTO(funcID);
+
+            return PartialView(result);
+        }
+
+        public ActionResult AjaxAddOrEditFunction(UserFunctionEditDTO dto)
+        {
+            var result = _funcService.AddOrEditFunction(dto);
+
+            return Json(result,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AjaxDeleteFunction(long funcID)
+        {
+            var result = _funcService.DeleteFunction(funcID);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
