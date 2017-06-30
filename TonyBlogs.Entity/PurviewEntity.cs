489301953108055677cd221a409d60f2a882bef9
Shinly <xinl_tong@foxmail.com>
2017-06-30 10:15:46 +0800

    1. 新增权限实体

---
+++ TonyBlogs.Entity/PurviewEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.DataAnnotations;
+
+namespace TonyBlogs.Entity
+{
+    public class PurviewEntity
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public long PurviewID { get; set; }
+
+        /// <summary>
+        /// 权限标题
+        /// </summary>
+        public string PurviewTitle { get; set; }
+
+        /// <summary>
+        /// 权限内容
+        /// </summary>
+        public string PurviewContent { get; set; }
+
+        /// <summary>
+        /// 权限功能组
+        /// </summary>
+        public string PurviewFuncIDs { get; set; }
+
+        /// <summary>
+        /// 录入时间
+        /// </summary>
+        public DateTime InsertTime { get; set; }
+    }
+}
