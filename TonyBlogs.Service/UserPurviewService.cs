afec4f9007e3cf4e74c8a76e33b1471acd71ab22
Shinly <xinl_tong@foxmail.com>
2017-06-30 10:21:44 +0800

    1. 新增用户权限数据层和服务层

---
+++ TonyBlogs.Service/UserPurviewService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TonyBlogs.Entity;
+using TonyBlogs.IService;
+
+namespace TonyBlogs.Service
+{
+    public class UserPurviewService : BaseService<PurviewEntity>, IUserPurviewService
+    {
+    }
+}
