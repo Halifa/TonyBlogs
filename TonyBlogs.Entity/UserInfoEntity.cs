f238e3a2e95d8b89350b564421f30531e2d14c59
Shinly <xinl_tong@foxmail.com>
2017-07-03 15:05:03 +0800

    1. 新增用户信息实体

---
+++ TonyBlogs.Entity/UserInfoEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.DataAnnotations;
+
+namespace TonyBlogs.Entity
+{
+    public class UserInfoEntity
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        [PrimaryKey]
+        [AutoIncrement]
+        public long UserID { get; set; }
+
+        /// <summary>
+        /// 登录账号
+        /// </summary>
+        public string LoginName { get; set; }
+
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string LoginPWD { get; set; }
+
+        /// <summary>
+        /// 真实姓名
+        /// </summary>
+        public string RealName { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int UserStatus { get; set; }
+
+        /// <summary>
+        /// 权限ID
+        /// </summary>
+        public long PurviewID { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime InsertTime { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+    }
+}
