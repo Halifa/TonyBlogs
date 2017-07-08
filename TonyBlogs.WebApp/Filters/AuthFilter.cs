7bd11fd8559690ff48d389abdf6066c916724a10
Shinly <xinl_tong@foxmail.com>
2017-07-08 10:14:54 +0800

    1. 修改功能菜单展示 2. 添加权限校验

---
+++ TonyBlogs.WebApp/Filters/AuthFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonyBlogs.WebApp.Filters
+{
+    public class AuthFilter : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
