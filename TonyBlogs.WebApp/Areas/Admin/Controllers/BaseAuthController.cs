f4b2134c44c222e78182beef72cfc8397a747ba6
Shinly <xinl_tong@foxmail.com>
2017-07-27 10:00:14 +0800

    1. 添加未跟踪文件

---
+++ TonyBlogs.WebApp/Areas/Admin/Controllers/BaseAuthController.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TonyBlogs.WebApp.Filters;
+
+namespace TonyBlogs.WebApp.Areas.Admin.Controllers
+{
+    [AuthFilter]
+    public class BaseAuthController : BaseController
+    {
+
+    }
+}
